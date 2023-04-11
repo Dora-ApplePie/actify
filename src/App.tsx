7a252ea6f54300a44e0f65bf8e4394e5abdfeeae
Dora-ApplePie <dasha0000golenko@gmail.com>
2023-04-11 16:19:06 +0300

    Create intro while app will be developing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import logo from "./assets/img/logo.svg"
 
 function App() {
   return (
     <div className="App">
-
+        <img alt={"logo"} src={logo}/>
+        <h2>Release soon...</h2>
     </div>
   );
 }
